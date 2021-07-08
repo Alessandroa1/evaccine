0217cab0d70e183fde4f33bde601d55e17cc577b
Alessandroa1 <54918597+Alessandroa1@users.noreply.github.com>
2021-07-07 21:09:44 -0300

    Add files via upload
    backend atualizado

--- BackendEvaccine/src/main/java/br/com/alessandro/sa/controller/AgendaController.java
+++ BackendEvaccine/src/main/java/br/com/alessandro/sa/controller/AgendaController.java
@@ -3,6 +3,7 @@ package br.com.alessandro.sa.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +15,8 @@ import br.com.alessandro.sa.domain.Agenda;
 import br.com.alessandro.sa.repository.AgendaRepository;
 
 @RestController
-@RequestMapping("/gruposprioritarios")
+@RequestMapping("/cadastrovacinacao")
+@CrossOrigin(origins = { "http://localhost:3000"})
 public class AgendaController {
 	
 	@Autowired
@@ -31,7 +33,7 @@ public class AgendaController {
 	}
 	
 	@PostMapping
-	public Agenda cadastarGrupoPrioritario (@RequestBody Agenda grupoPrioritario) {
-		return agendaRepository.save(grupoPrioritario);
+	public Agenda cadastarVacinacao (@RequestBody Agenda agendamento) {
+		return agendaRepository.save(agendamento);
 	}
 }
--- BackendEvaccine/src/main/java/br/com/alessandro/sa/domain/Agenda.java
+++ BackendEvaccine/src/main/java/br/com/alessandro/sa/domain/Agenda.java
@@ -12,41 +12,57 @@ public class Agenda {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 	
-	private String nome;
+	private String nomePaciente;
 	
-	private String descricao;
-
-	public Long getCodigo() {
-		return codigo;
+	private String dataAgendamento;
+	
+	private String horarioAgendamento;
+	
+	private String nomeVacina;
+	
+	public String getDataAgendamento() {
+		return dataAgendamento;
 	}
 
-	public void setCodigo(Long codigo) {
-		this.codigo = codigo;
+	public void setDataAgendamento(String dataAgendamento) {
+		this.dataAgendamento = dataAgendamento;
 	}
 
-	public String getNome() {
-		return nome;
+	public String getHorarioAgendamento() {
+		return horarioAgendamento;
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
+	public void setHorarioAgendamento(String horarioAgendamento) {
+		this.horarioAgendamento = horarioAgendamento;
 	}
 
-	public String getDescricao() {
-		return descricao;
+	public String getNomeVacina() {
+		return nomeVacina;
 	}
 
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
+	public void setNomeVacina(String nomeVacina) {
+		this.nomeVacina = nomeVacina;
 	}
 
+	private String dose;
+
+	private String nomeDoPaciente;
+
+	private String nomeDaVacina;
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
-		result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
-		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((dataAgendamento == null) ? 0 : dataAgendamento.hashCode());
+		result = prime * result + ((dose == null) ? 0 : dose.hashCode());
+		result = prime * result + ((horarioAgendamento == null) ? 0 : horarioAgendamento.hashCode());
+		result = prime * result + ((nomeDaVacina == null) ? 0 : nomeDaVacina.hashCode());
+		result = prime * result + ((nomeDoPaciente == null) ? 0 : nomeDoPaciente.hashCode());
+		result = prime * result + ((nomePaciente == null) ? 0 : nomePaciente.hashCode());
+		result = prime * result + ((nomeVacina == null) ? 0 : nomeVacina.hashCode());
 		return result;
 	}
 
@@ -64,17 +80,99 @@ public class Agenda {
 				return false;
 		} else if (!codigo.equals(other.codigo))
 			return false;
-		if (descricao == null) {
-			if (other.descricao != null)
+		if (dataAgendamento == null) {
+			if (other.dataAgendamento != null)
 				return false;
-		} else if (!descricao.equals(other.descricao))
+		} else if (!dataAgendamento.equals(other.dataAgendamento))
 			return false;
-		if (nome == null) {
-			if (other.nome != null)
+		if (dose == null) {
+			if (other.dose != null)
 				return false;
-		} else if (!nome.equals(other.nome))
+		} else if (!dose.equals(other.dose))
+			return false;
+		if (horarioAgendamento == null) {
+			if (other.horarioAgendamento != null)
+				return false;
+		} else if (!horarioAgendamento.equals(other.horarioAgendamento))
+			return false;
+		if (nomeDaVacina == null) {
+			if (other.nomeDaVacina != null)
+				return false;
+		} else if (!nomeDaVacina.equals(other.nomeDaVacina))
+			return false;
+		if (nomeDoPaciente == null) {
+			if (other.nomeDoPaciente != null)
+				return false;
+		} else if (!nomeDoPaciente.equals(other.nomeDoPaciente))
+			return false;
+		if (nomePaciente == null) {
+			if (other.nomePaciente != null)
+				return false;
+		} else if (!nomePaciente.equals(other.nomePaciente))
+			return false;
+		if (nomeVacina == null) {
+			if (other.nomeVacina != null)
+				return false;
+		} else if (!nomeVacina.equals(other.nomeVacina))
 			return false;
 		return true;
+	}
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDataDoAgendamento() {
+		return dataAgendamento;
+	}
+
+	public void setDataDoAgendamento(String dataDoAgendamento) {
+		this.dataAgendamento = dataDoAgendamento;
+	}
+
+	public String getHorarioDoAgendamento() {
+		return horarioAgendamento;
+	}
+
+	public void setHorarioDoAgendamento(String horarioDoAgendamento) {
+		this.horarioAgendamento = horarioDoAgendamento;
+	}
+
+
+	public String getNomeDaVacina() {
+		return nomeDaVacina;
+	}
+
+	public void setNomeDaVacina(String nomeDaVacina) {
+		this.nomeDaVacina = nomeDaVacina;
+	}
+
+	public String getDose() {
+		return dose;
+	}
+
+	public void setDose(String dose) {
+		this.dose = dose;
+	}
+
+	public String getNomeDoPaciente() {
+		return nomeDoPaciente;
+	}
+
+	public void setNomeDoPaciente(String nomeDoPaciente) {
+		this.nomeDoPaciente = nomeDoPaciente;
+	}
+
+	public String getNomePaciente() {
+		return nomePaciente;
+	}
+
+	public void setNomePaciente(String nomePaciente) {
+		this.nomePaciente = nomePaciente;
 	}
 	
 	
